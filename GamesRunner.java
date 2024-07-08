2ba3cc92bd177eb9228a9eca19b0d5894ed5031a
madhukesalkar <madhugk8147@gmail.com>
2024-07-08 10:33:23 +0530

    commiting separate Method related java code

---
+++ GamesRunner.java
@@ -0,0 +1,10 @@
+class GamesRunner{
+public static void main(String []volyball){
+
+    System.out.println("Main started");
+    Games.play();
+    System.out.println("Main Ended");
+
+
+}
+}
---
+++ PodiumRunner.java
@@ -0,0 +1,10 @@
+class PodiumRunner{
+public static void main(String []){
+
+    System.out.println("Main started");
+    Projector.hold();
+    System.out.println("Main Ended");
+
+
+}
+}
